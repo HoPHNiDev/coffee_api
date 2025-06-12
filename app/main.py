378bde8a6aa211d485e80e0a9616014608fe76b9
HoPHNiDev <hophniagent@gmail.com>
2025-06-12 21:13:55 +0500

    add FastAPI application setup with CORS middleware and Uvicorn server integration

--- app/main.py
+++ app/main.py
@@ -1,6 +1,28 @@
-from core.config.logging import LoggingSettings
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-def create_application():
-    ...
+from core.config import settings
 
-create_application()
+
+def create_application() -> FastAPI:
+    """
+    Создает и настраивает экземпляр приложения FastAPI.
+    """
+    app = FastAPI(**settings.app_params)
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=settings.CORS_ORIGINS,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+    return app
+
+
+app = create_application()
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", **settings.uvicorn_params)
